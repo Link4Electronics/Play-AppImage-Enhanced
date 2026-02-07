2390b9fc65458cb324672b3defe28a215bc55208
Link4Electronics <link4electronics@gmail.com>
2026-02-07 10:44:00 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,8 +8,8 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=DUMMY
-export DESKTOP=DUMMY
+export ICON=/usr/share/pixmaps/Play.png
+export DESKTOP=/usr/share/applications/Play.desktop
 
 # Deploy dependencies
 quick-sharun /usr/bin/Play
