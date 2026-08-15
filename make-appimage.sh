c0b2f4a2df36321c28f694fde45467affb06c551
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:49:39 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q play-emu-git | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q play-emu-git | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
@@ -18,7 +18,9 @@ export DEPLOY_VULKAN=1
 # Deploy dependencies
 quick-sharun /usr/bin/Play
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
